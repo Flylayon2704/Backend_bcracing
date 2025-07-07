fad540ebe81ec27b92d98606960021e842af161f
Diego Pineda <zentoo31@gmail.com>
2025-07-06 22:12:35 -0500

    feat: initialize database connection in app setup

--- app.mjs
+++ app.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors"; 
 import authRoutes from "./routes/authRoutes.mjs";
+import connectDB from "./config/db.mjs";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+connectDB(); 
 
 const PORT = process.env.PORT || 3000;
 
