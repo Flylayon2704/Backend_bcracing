9aa0cb741ff30962067465f1272120280b70ac4e
Diego Pineda <zentoo31@gmail.com>
2025-07-08 22:17:07 -0500

    feat: implement ProductService with CRUD operations for products

---
+++ services/productService.mjs
@@ -0,0 +1,36 @@
+import productSchema from "../models/productSchema.mjs";
+
+export class ProductService {
+    static async createProduct(productData) {
+        const existingProduct = await productSchema.findOne({ codigo: productData.codigo });
+        if (existingProduct) throw new Error('El producto ya existe');
+
+        const product = new productSchema(productData);
+        await product.save();
+        return product;
+    }
+
+    static async getAllProducts() {
+        return await productSchema
+                        .find()
+                        .limit(100);
+    }
+
+    static async getProductById(id) {
+        const product = await productSchema.findById(id);
+        if (!product) throw new Error('Producto no encontrado');
+        return product;
+    }
+
+    static async updateProduct(id, updateData) {
+        const updatedProduct = await productSchema.findByIdAndUpdate(id, updateData, { new: true });
+        if (!updatedProduct) throw new Error('Producto no encontrado');
+        return updatedProduct;
+    }
+
+    static async deleteProduct(id) {
+        const deletedProduct = await productSchema.findByIdAndDelete(id);
+        if (!deletedProduct) throw new Error('Producto no encontrado');
+        return deletedProduct;
+    }
+}
